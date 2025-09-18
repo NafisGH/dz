069510e78d65e5a80458786947d82e630e299a6a
GarifullinNKh <GarifullinNKh>
2025-09-18 16:55:43 +0500

    2_DZ
    Сделана домашка Компоненты

--- dz-vue_pinia/src/App.vue
+++ dz-vue_pinia/src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
+import Button from "./components/Button.vue";
 const correctDate = new Date();
 </script>
 
 <template>
   <h2>{{ correctDate }}</h2>
+  <Button>Кнопка</Button>
 </template>
 
 <style scoped></style>
---
+++ dz-vue_pinia/src/components/Button.vue
@@ -0,0 +1,24 @@
+<script setup></script>
+<template>
+  <button class="button">
+    <slot />
+  </button>
+</template>
+<style scoped>
+.button {
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  width: 335px;
+  height: 53px;
+  gap: 9px;
+  border: none;
+  border-radius: 100px;
+  background: var(--color-background-blue);
+  padding: 16px 10px;
+  font-family: var(--font);
+  font-size: 20px;
+  color: var(--primary);
+  cursor: pointer;
+}
+</style>
